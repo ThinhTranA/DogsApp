605692df03b07c50e48d099b58998f107c7b06c7
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2019-11-09 20:03:56 +0800

    Navigation fuction

--- app/src/main/java/com/visualsharp/dogsapp/view/MainActivity.java
+++ app/src/main/java/com/visualsharp/dogsapp/view/MainActivity.java
@@ -1,6 +1,10 @@
 package com.visualsharp.dogsapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
@@ -8,9 +12,19 @@ import com.visualsharp.dogsapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private NavController navController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navController = Navigation.findNavController(this, R.id.fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController, (DrawerLayout) null);
     }
 }
