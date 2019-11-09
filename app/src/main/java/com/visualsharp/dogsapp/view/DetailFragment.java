1de7afa7a28e4c3d00aed2bf6ec58873c216e476
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2019-11-10 07:43:44 +0800

    Arguments and Animations

--- app/src/main/java/com/visualsharp/dogsapp/view/DetailFragment.java
+++ app/src/main/java/com/visualsharp/dogsapp/view/DetailFragment.java
@@ -3,16 +3,33 @@ package com.visualsharp.dogsapp.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.visualsharp.dogsapp.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class DetailFragment extends Fragment {
 
+    @BindView(R.id.floatingActionButton2)
+    FloatingActionButton fab;
+
+    @BindView(R.id.textView2)
+    TextView tv2;
+
+    private int dogUuid;
 
     public DetailFragment() {
     }
@@ -21,7 +38,25 @@ public class DetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+        ButterKnife.bind(this,view);
+        return view;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if(getArguments() != null){
+            dogUuid = DetailFragmentArgs.fromBundle(getArguments()).getDogUuid();
+            tv2.setText(String.valueOf(dogUuid));
+        }
+        fab.setOnClickListener(v -> {
+            onGoToList();
+        });
+    }
+
+    private void onGoToList() {
+        NavDirections action = DetailFragmentDirections.actionList();
+        Navigation.findNavController(fab).navigate(action);
+    }
 }
