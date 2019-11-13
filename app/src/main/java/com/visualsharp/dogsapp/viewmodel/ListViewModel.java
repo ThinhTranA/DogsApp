74dbb46af81867517984b416e309e1112131b6da
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2019-11-13 18:28:27 +0800

    Get remote data

--- app/src/main/java/com/visualsharp/dogsapp/viewmodel/ListViewModel.java
+++ app/src/main/java/com/visualsharp/dogsapp/viewmodel/ListViewModel.java
@@ -7,9 +7,16 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
 
 import com.visualsharp.dogsapp.model.DogBreed;
+import com.visualsharp.dogsapp.model.DogsApiService;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
 
 public class ListViewModel extends AndroidViewModel {
 
@@ -17,24 +24,45 @@ public class ListViewModel extends AndroidViewModel {
     public MutableLiveData<Boolean> dogLoadError = new MutableLiveData<Boolean>();
     public MutableLiveData<Boolean> loading = new MutableLiveData<Boolean>();
 
+    private DogsApiService dogsService = new DogsApiService();
+    private CompositeDisposable disposable = new CompositeDisposable();
+
     public ListViewModel(@NonNull Application application) {
         super(application);
     }
 
     public void refresh(){
-        DogBreed dog1 = new DogBreed("1", "corgi", "15 years", "", "", "", "", 0);
-        DogBreed dog2 = new DogBreed("2", "rotwailer", "10 years", "", "", "", "", 0);
-        DogBreed dog3 = new DogBreed("3", "labrador", "13 years", "", "", "", "", 0);
+        fetchFromRemote();
+    }
 
-        ArrayList<DogBreed> dogsList = new ArrayList<>();
-        dogsList.add(dog1);
-        dogsList.add(dog2);
-        dogsList.add(dog3);
+    private void fetchFromRemote(){
+        loading.setValue(true);
+        disposable.add(
+        dogsService.getDogs()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableSingleObserver<List<DogBreed>>() {
+                    @Override
+                    public void onSuccess(List<DogBreed> dogBreeds) {
+                        dogs.setValue(dogBreeds);
+                        dogLoadError.setValue(false);
+                        loading.setValue(false);
+                    }
 
-        dogs.setValue(dogsList);
-        dogLoadError.setValue(false);
-        loading.setValue(false);
+                    @Override
+                    public void onError(Throwable e) {
+                        dogLoadError.setValue(true);
+                        loading.setValue(false);
+                        e.printStackTrace();
+                    }
+                })
+        );
 
     }
 
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        disposable.clear();
+    }
 }
