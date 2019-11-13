b81b4a254c1519b4c65cd4b7ada11098457fc92c
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2019-11-13 17:52:55 +0800

    Setting up Retrofit

--- app/src/main/java/com/visualsharp/dogsapp/model/DogBreed.java
+++ app/src/main/java/com/visualsharp/dogsapp/model/DogBreed.java
@@ -1,13 +1,28 @@
 package com.visualsharp.dogsapp.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class DogBreed {
+    @SerializedName("id")
     public String breedId;
+
+    @SerializedName("name")
     public String dogBreed;
+
+    @SerializedName("life_span")
     public String lifeSpan;
+
+    @SerializedName("breed_group")
     public String breedGroup;
+
+    @SerializedName("bred_for")
     public String bredFor;
+
     public String temperament;
+
+    @SerializedName("url")
     public String imageUrl;
+
     public int uui;
 
     public DogBreed(String breedId, String dogBreed, String lifeSpan, String breedGroup, String bredFor, String temperament, String imageUrl, int uui) {
